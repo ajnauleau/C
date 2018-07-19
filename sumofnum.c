e4e90134523c72167a9c0f43411cdcbec9cf4d05
Antoine Nauleau <ajnauleau@gmail.com>
2018-07-19 20:24:45 +0200

    Work on sumofsum

---
+++ sumofnum.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+
+void FirstFactorial(int num[]) { 
+  
+  int n;
+  int num;
+  
+  n = n*(n+1)/2;
+  
+  return n;
+  
+  num = n;
+   
+  printf("%d", num); 
+  
+  return 0;
+            
+}
+
+int main(void) { 
+  
+  // keep this function call here
+  FirstFactorial(gets(stdin));
+  return 0;
+    
+}
