963ac0066e9c9d8f6e4a83c917822648a613be2a
Antoine Nauleau <ajnauleau@gmail.com>
2018-07-28 17:34:48 +0200

    Add linkedlist.cs

---
+++ linkedlist.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+typedef struct node{
+    int data;
+    struct node* next;
+}
+
+node* create(int data,node* next)
+{
+    node* new_node = (node*)malloc(sizeof(node));
+    if(new_node == NULL)
+    {
+        printf("Error creating a new node.\n");
+        exit(0);
+    }
+    new_node->data = data;
+    new_node->next = next;
+ 
+    return new_node;
+}
